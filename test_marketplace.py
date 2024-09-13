7a20b72a053e9932deae71b36f35f1b30b6a5e77
Vladislav Bosomykin <86566403+QACAMYRAI@users.noreply.github.com>
2024-09-13 11:00:14 +0300

    Refactor

--- test_marketplace.py
+++ test_marketplace.py
@@ -1,7 +1,5 @@
-import pytest
 from playwright.sync_api import sync_playwright, expect
-import locators
-
+import locators 
 
 def test_buy_something():
     with sync_playwright() as playwright:
@@ -9,23 +7,37 @@ def test_buy_something():
         browser = chromium.launch(headless=False, slow_mo=1000)
         context = browser.new_context()
         page = context.new_page()
-        page.goto(locators.URL)
-        page.get_by_placeholder(locators.INPUT_USERNAME).fill(locators.login)
-        page.get_by_placeholder(locators.INPUT_PASSWORD).fill(locators.password)
-        page.locator(locators.LOGIN).click()
-        page.locator(locators.PRODUCT_FOR_TEST).click()
-        page.locator(locators.ADD_TO_CART).click()
-        page.locator(locators.SHOPING_CART).click()
-        page.locator(locators.CHECKOUT).click()
-        page.get_by_placeholder(locators.FIRST_NAME).fill(locators.first_name)
-        page.get_by_placeholder(locators.LAST_NAME).fill(locators.last_name)
-        page.get_by_placeholder(locators.POSTAL_CODE).fill(locators.postal_code)
-        page.locator(locators.CONTINUE).click()
-        page.locator(locators.FINISH).click()
 
-        expect(page.locator(locators.COMPLETE_HEADER)).to_have_text("Thank you for your order!")
+        try:
+            # Переход на страницу
+            page.goto(locators.URL)
 
+            # Ввод логина и пароля
+            page.get_by_placeholder(locators.INPUT_USERNAME).fill(locators.login)
+            page.get_by_placeholder(locators.INPUT_PASSWORD).fill(locators.password)
+            page.locator(locators.LOGIN).click()
 
-        context.close()
-        browser.close()
+            # Выбор продукта и добавление в корзину
+            page.locator(locators.PRODUCT_FOR_TEST).click()
+            page.locator(locators.ADD_TO_CART).click()
+            page.locator(locators.SHOPING_CART).click()
+            page.locator(locators.CHECKOUT).click()
+
+            # Ввод данных для оформления заказа
+            page.get_by_placeholder(locators.FIRST_NAME).fill(locators.first_name)
+            page.get_by_placeholder(locators.LAST_NAME).fill(locators.last_name)
+            page.get_by_placeholder(locators.POSTAL_CODE).fill(locators.postal_code)
+            page.locator(locators.CONTINUE).click()
+            page.locator(locators.FINISH).click()
+
+            # Проверка успешного завершения заказа
+            expect(page.locator(locators.COMPLETE_HEADER)).to_have_text("Thank you for your order!")
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+        finally:
+            # Закрытие контекста и браузера
+            context.close()
+            browser.close()
 
